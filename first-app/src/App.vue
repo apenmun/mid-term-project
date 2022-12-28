70658687333bec4fe40054964f5bb7ffb3854a11
Apenmun <apenmun@gmail.com>
2022-12-28 09:55:40 +0100

    <problems with padding or margin>

---
+++ first-app/src/App.vue
@@ -0,0 +1,14 @@
+<template> <h2>{{message}}</h2> 
+<input type="email" ref="email">
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Mundo");
+
+const email = ref("write your email");
+
+</script>
+
+<style></style>
