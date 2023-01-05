4f832f277bc5607c078b93f600ac317b45d228ee
Apenmun <apenmun@gmail.com>
2023-01-05 09:02:14 +0100

    <last part>

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hola')
+const obtenerPokemons = async () => {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await res.json();
+    console.log(data.results);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+obtenerPokemons();
+
+const HTMLResponse = document.querySelector('#app');
+
+//Button para Contact us
+document.getElementById("btnContact").addEventListener("click", e=>{window.location.href = "contactus.html"})
+
+
+
+
